381f29e631255cd9d33ff5892f1688926d98b577
roshanNaik07 <roshannaik202055>
2025-07-14 21:48:47 +0530

    Creating Map and fetching the value using key

---
+++ Park/src/com/xworkz/park/Map/USN.java
@@ -0,0 +1,21 @@
+package com.xworkz.park.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class USN {
+
+    public static void main(String[] args) {
+
+        Map<String ,String> map= new HashMap<>();
+        map.put("Roshan","4sh21cs114");
+        map.put("arjun","4sh21cs234");
+        map.put("adhitya","4sh21cs111");
+
+        System.out.println(map.get("Roshan"));
+        System.out.println(map.get("arjun"));
+        System.out.println(map.get("mahesh"));
+
+    }
+
+}
